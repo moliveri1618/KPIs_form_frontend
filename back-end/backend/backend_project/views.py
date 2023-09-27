7baf4fd652e7c28233f8832e1e415f5878dc1437
moliveri1618 <lastiada1@gmail.com>
2023-09-27 14:25:11 +0100

    commit

--- back-end/backend/backend_project/views.py
+++ back-end/backend/backend_project/views.py
@@ -4,3 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 def homepage(request):
     return HttpResponse('Hello World')
+
+
+# Create your views here.
+def yo(request):
+    return HttpResponse("yooyoyyoy")
