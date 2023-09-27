ae8b14aeca21f3d3f40da8f2c416ff059ca939d4
moliveri1618 <lastiada1@gmail.com>
2023-09-27 10:13:00 +0100

    commit

---
+++ front-end/front-end/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './Components/Form'
+import BasicButtons from './Components/BasicButtons';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Form /> */}
+        <BasicButtons />
+    </div>
+  );
+}
+
+export default App;
