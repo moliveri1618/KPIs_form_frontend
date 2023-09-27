c73e664e4dd1c03046724340c259e420ca2556bb
moliveri1618 <lastiada1@gmail.com>
2023-09-27 10:35:33 +0100

    commit

--- front-end/front-end/src/App.js
+++ front-end/front-end/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './Components/Form'
 import BasicButtons from './Components/BasicButtons';
+import AddressForm from './Components/AddressForm';
+import CheckoutPage from './Components/Checkout';
 
 function App() {
   return (
     <div className="App">
         {/* <Form /> */}
         <BasicButtons />
+        {/* <AddressForm /> */}
+        {/* <CheckoutPage /> */}
     </div>
   );
 }
