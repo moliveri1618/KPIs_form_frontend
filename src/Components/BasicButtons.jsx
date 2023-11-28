4542834dd2df3cef8eefd2e329bcce178af2cd42
moliveri1618 <lastiada1@gmail.com>
2023-11-28 13:31:46 +0000

    commit

--- src/Components/BasicButtons.jsx
+++ src/Components/BasicButtons.jsx
@@ -53,111 +53,18 @@ export default function BasicButtons() {
       autoComplete="off"
     >
     <div style={{ display: 'flex', alignItems: 'center', paddingBottom: '20px'  }}>
-      <Avatar sx={{ bgcolor: '#006400', marginRight: '8px' }}>
+      {/* <Avatar sx={{ bgcolor: '#006400', marginRight: '8px' }}>
         <AssignmentTurnedInRoundedIcon />
-      </Avatar>
-      <Typography component="h1" variant="h5">
-        Doi Form
+      </Avatar> */}
+      <Typography component="h1" variant="h3" style={{ fontFamily: 'Sedan-Regular', fontWeight: 400 }}>
+        Submit your DOI
       </Typography>
-      </div>
+    </div>
+    <div style={{ display: 'flex', gap: '10px',  width: '100vw', height: '100vh', justifyContent: 'center'}}>
+      <TextField id="outlined-search" label="Search field" type="search" style={{ width: '40%' }} />
+      <TextField id="outlined-search" label="Search field" type="search" style={{ width: '40%' }} />
+    </div>
       <div>
-        <TextField
-          required
-          id="outlined-required"
-          label="Required"
-          // defaultValue="Hello World"
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-        <TextField
-          disabled
-          id="outlined-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-        />
-        <TextField
-          id="outlined-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        <TextField
-          id="outlined-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <TextField id="outlined-search" label="Search field" type="search" />
-        <TextField
-          id="outlined-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-        />
-      </div>
-      <div>
-        <TextField
-          required
-          id="filled-required"
-          label="Required"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          disabled
-          id="filled-disabled"
-          label="Disabled"
-          defaultValue="Hello World"
-          variant="filled"
-        />
-        <TextField
-          id="filled-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="filled"
-        />
-        <TextField
-          id="filled-read-only-input"
-          label="Read Only"
-          defaultValue="Hello World"
-          InputProps={{
-            readOnly: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-number"
-          label="Number"
-          type="number"
-          InputLabelProps={{
-            shrink: true,
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="filled-search"
-          label="Search field"
-          type="search"
-          variant="filled"
-        />
-        <TextField
-          id="filled-helperText"
-          label="Helper text"
-          defaultValue="Default Value"
-          helperText="Some important text"
-          variant="filled"
-        />
         <div style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
         <Button type="submit" variant="contained" color="primary">
           Submit
