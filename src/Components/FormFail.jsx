c4e9b9ce49158f92b4f2e1c4ddd23098c6e3857e
moliveri1618 <lastiada1@gmail.com>
2023-11-29 18:57:17 +0000

    commit

--- src/Components/FormFail.jsx
+++ src/Components/FormFail.jsx
@@ -10,7 +10,63 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 
+
 export default function FormFail() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [volume, setVolume] = useState('');
+  const [issn, setISSN] = useState('');
+  const [url, setUrl] = useState('');
+  const [number, setNumber] = useState('');
+  const [journal, setJournal] = useState('');
+  const [publisher, setPublisher] = useState('');
+  const [pages, setPages] = useState('');
+  const [year, setYear] = useState('');
+  const [month, setMonth] = useState('');
+
+  const handleChangeTitle = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const handleChangeAuthor= (event) => {
+    setAuthor(event.target.value);
+  };
+
+  const handleChangeVolume = (event) => {
+    setVolume(event.target.value);
+  };
+  
+  const handleChangeISSN = (event) => {
+    setISSN(event.target.value);
+  };
+  
+  const handleChangeUrl = (event) => {
+    setUrl(event.target.value);
+  };
+  
+  const handleChangeNumber = (event) => {
+    setNumber(event.target.value);
+  };
+  
+  const handleChangeJournal = (event) => {
+    setJournal(event.target.value);
+  };
+  
+  const handleChangePublisher = (event) => {
+    setPublisher(event.target.value);
+  };
+  
+  const handleChangePages = (event) => {
+    setPages(event.target.value);
+  };
+  
+  const handleChangeYear = (event) => {
+    setYear(event.target.value);
+  };
+  
+  const handleChangeMonth = (event) => {
+    setMonth(event.target.value);
+  };
 
   const notify = (varValue) => {
     toast.error(`${varValue} could not be found`, {
@@ -59,7 +115,6 @@ export default function FormFail() {
     let doi = ''
     if (doiMatch && doiMatch[1]) {
       doi = doiMatch[1];
-      console.log(doi);
     }
     notify(doi)
 
@@ -94,8 +149,8 @@ export default function FormFail() {
                     justifyContent: 'center',
                     paddingTop: '40px', 
                     paddingBottom: '20px'}}> 
-        <TextField id="a" label="Title" type="search" style={{ width: '40%', height: '40px',margin: '0'}} required/>
-        <TextField id="a" label="Author" type="search" style={{ width: '40%', height: '40px',margin: '0'}} required/>
+        <TextField id="a" label="Title" type="search" style={{ width: '40%', height: '40px',margin: '0'}} onChange={handleChangeTitle} required/>
+        <TextField id="a" label="Author" type="search" style={{ width: '40%', height: '40px',margin: '0'}} onChange={handleChangeAuthor} required/>
       </div>
       <div style={{ display: 'flex', 
                     gap: '10px',  
@@ -103,9 +158,9 @@ export default function FormFail() {
                     justifyContent: 'center',
                     paddingTop: '40px', 
                     paddingBottom: '20px'}}> 
-        <TextField id="b" label="Volume" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} required/>
-        <TextField id="c" label="ISSN" type="search" style={{ width: '26%', height: '40px',margin: '0'}} required/>
-        <TextField id="d" label="Url" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} required/>
+        <TextField id="b" label="Volume" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} onChange={handleChangeVolume} required/>
+        <TextField id="c" label="ISSN" type="search" style={{ width: '26%', height: '40px',margin: '0'}} onChange={handleChangeISSN} required/>
+        <TextField id="d" label="Url" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} onChange={handleChangeUrl} required/>
       </div>
       <div style={{ display: 'flex', 
                     gap: '10px',  
@@ -113,9 +168,9 @@ export default function FormFail() {
                     justifyContent: 'center',
                     paddingTop: '40px', 
                     paddingBottom: '20px'}}> 
-        <TextField id="e" label="Number" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} required/>
-        <TextField id="f" label="Journal" type="search" style={{ width: '26%', height: '40px',margin: '0'}} required/>
-        <TextField id="g" label="Publisher" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} required/>
+        <TextField id="e" label="Number" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} onChange={handleChangeNumber} required/>
+        <TextField id="f" label="Journal" type="search" style={{ width: '26%', height: '40px',margin: '0'}} onChange={handleChangeJournal} required/>
+        <TextField id="g" label="Publisher" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} onChange={handleChangePublisher} required/>
       </div>
       <div style={{ display: 'flex', 
                     gap: '10px',  
@@ -123,9 +178,9 @@ export default function FormFail() {
                     justifyContent: 'center',
                     paddingTop: '40px', 
                     paddingBottom: '60px'}}> 
-        <TextField id="h" label="Pages" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} required/>
-        <TextField id="i" label="Year" type="search" style={{ width: '26%', height: '40px',margin: '0'}} required/>
-        <TextField id="l" label="Month" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} required/>
+        <TextField id="h" label="Pages" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} onChange={handleChangePages} required/>
+        <TextField id="i" label="Year" type="search" style={{ width: '26%', height: '40px',margin: '0'}} onChange={handleChangeYear} required/>
+        <TextField id="l" label="Month" type="search" style={{ width: '26.5%', height: '40px',margin: '0'}} onChange={handleChangeMonth} required/>
       </div>
       <div style={{ display: 'flex', justifyContent: 'right', width: '83vw' }}>
         <Link to="/KPIs_form_frontend">
