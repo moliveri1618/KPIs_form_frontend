0bf6b62c44aa9fa4d54d9301c32d92d1499d600d
moliveri1618 <lastiada1@gmail.com>
2023-11-29 15:58:35 +0000

    validation form

--- src/App.js
+++ src/App.js
@@ -2,22 +2,21 @@ import './App.css';
 import StartingForm from './Components/StartingForm';
 import FormFail from './Components/FormFail'
 import FormSuccess from './Components/FormSuccess'
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import React, {useEffect } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-import BasicButtons from './Components/BasicButtons';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 
 function App() {
 
   return (
-    <div className="App">
-        {/* <Form /> */}
-        <BasicButtons />
-        {/* <Form /> */}
-        {/* <AddressForm /> */}
-        {/* <CheckoutPage /> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/KPIs_form_frontend" element={<StartingForm/>} />
+        <Route path="/KPIs_form_frontend/fail" element={<FormFail/>} />
+        <Route path="/KPIs_form_frontend/success" element={<FormSuccess/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/Components/StartingForm.jsx
+++ src/Components/StartingForm.jsx
@@ -15,30 +15,49 @@ import { useNavigate } from 'react-router-dom';
 export default function StartingForm() {
   const [data, setData] = useState(null);
   const [group, setGroup] = React.useState('');
+  const [doi, setDoi] = useState('');
+  const [isValid, setIsValid] = useState(false);
   const groups = ['10', 'ffff', 'dsfdf'];
   const navigate = useNavigate();
 
   const handleChange = (event) => {
     setGroup(event.target.value);
+    validateForm(event.target.value, doi);
+
   };
 
-  const handleApiTest = async () => {
-    let api = ''
-    try {
-      if (process.env.NODE_ENV !== 'production') {
-        api = 'http://' + process.env.REACT_APP_API_URL_DEV + '/stoca'
-      } else {
-        api = 'http://' + process.env.REACT_APP_API_URL_PROD + '/stoca'
+  const handleDoiChange = (event) => {
+    setDoi(event.target.value);
+    validateForm(group, event.target.value);
+  };
+
+  const validateForm = (groupValue, doiValue) => {
+    // Add your validation logic here
+    const isValidForm = groupValue.trim() !== '' && doiValue.trim() !== '';
+    setIsValid(isValidForm);
+  };
+
+  const handleApiTest = async (event) => {
+    event.preventDefault();
+    if (isValid) {
+      let api = ''
+      try {
+        if (process.env.NODE_ENV !== 'production') {
+          api = 'http://' + process.env.REACT_APP_API_URL_DEV + '/stoca'
+        } else {
+          api = 'http://' + process.env.REACT_APP_API_URL_PROD + '/stoca'
+        }
+        const response = axios.post(api)
+        setData(response.data);
+        console.log(response.data)
+        if (data !== 'DOI doesnt exists') {
+          navigate('/KPIs_form_frontend/fail');
+        } 
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-      const response = axios.post(api)
-      setData(response.data);
-      console.log(response.data)
-      if (data !== 'DOI doesnt exists') {
-        navigate('/fail');
-      } 
-    } catch (error) {
-      console.error('Error fetching data:', error);
     }
+
   };
 
   useEffect(() => {
@@ -85,7 +104,7 @@ export default function StartingForm() {
           ))}
           </Select>
         </FormControl>
-        <TextField id="outlined-search" label="DOI" type="search" style={{ width: '40%', height: '40px',margin: '0'}} />
+        <TextField id="outlined-search" label="DOI" type="search" style={{ width: '40%', height: '40px',margin: '0'}} value={doi} onChange={handleDoiChange}/>
       </div>
       <div style={{ display: 'flex', justifyContent: 'right', width: '80.5vw' }}>
         <Button type="submit" variant="contained" color="primary" onClick={handleApiTest} style={{ width: '15%' }}>
