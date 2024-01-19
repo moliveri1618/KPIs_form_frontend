26cd7c46fcc6061e781947f1acfdf5ac5c6e0454
moliveri1618 <lastiada1@gmail.com>
2024-01-19 15:19:45 +0000

    commit

--- src/Components/CopyRight.jsx
+++ src/Components/CopyRight.jsx
@@ -1,20 +1,31 @@
-import React, {useState, useEffect } from 'react';
-import Container from '@mui/material/Container';
+import React from 'react';
 import Typography from '@mui/material/Typography';
 
+function Copyright() {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        bottom: 0,
+        left: 0,
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: '#f8f8f8', // optional background color
+        padding: '8px', // optional padding
+      }}
+    >
+      <Typography variant="body2" color="text.secondary" align="center" style={{ marginBottom: '8px' }}>
+        Developed by Pedro Cruz, Inês Isidro and Mauro Oliveri.
+      </Typography>
+      <Typography variant="body2" color="text.secondary" align="center" style={{ marginTop: '0' }}>
+        {'Copyright © '}
+        <strong>{'2023 iBET - Instituto de Biologia Experimental e Tecnológica.'}{' '}</strong>
+      </Typography>
+    </div>
+  );
+}
 
-function Copyright(props) {
-    return (
-      <Container>
-        <Typography variant="body2" color="text.secondary" align="center" {...props} style={{ }}>
-          Developed by Pedro Cruz, Inês Isidro and Mauro Oliveri.
-        </Typography>
-        <Typography variant="body2" color="text.secondary" align="center" {...props} style={{ marginTop: '0' }}>
-          {'Copyright © '}
-          <strong>{'2023 iBET - Instituto de Biologia Experimental e Tecnológica.'}{' '}</strong>
-        </Typography>
-      </Container>
-    );
-  }
-  
-  export default Copyright;
+export default Copyright;
--- src/Components/SpinnerLoader.jsx
+++ src/Components/SpinnerLoader.jsx
@@ -9,14 +9,14 @@ export default function SPLoader() {
     useEffect(() => {
         setTimeout(() => {
             setShowImg(false)
-        }, 8000)
+        }, 50000)
     }, [])
 
     return (
         <>
             <div style={{ display: 'flex', alignItems: 'center' }}>
-                <p>Fetching Data...</p>
-                {showImg ? (<img src={aa} style={{ width: '60px', height: '60px' }}/>) : ( <h3>{text}</h3>)}
+                <p>Searching DOI ...</p>
+                {showImg ? (<img src={aa} style={{ width: '100px', height: '100px' }}/>) : ( <h3>{text}</h3>)}
             </div>
         </>
     )
