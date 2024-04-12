92c525ad6658a119a8fa616d3c35e55afff7b775
moliveri1618 <lastiada1@gmail.com>
2024-04-12 21:23:49 +0100

    commit dcan!!

--- src/Components/StartingForm.jsx
+++ src/Components/StartingForm.jsx
@@ -28,22 +28,19 @@ export default function StartingForm() {
   const [open, setOpen] = useState(false);
   const [textDialog, setTextDialog] = useState('Select Groups');
   const [projectCodes, setProjectCodes] = useState('');
+  const [isSubscribed, setIsSubscribed] = useState(true);
+  const [isValidRedirect, setIsValidRedirect] = useState('');
 
-  const check_token_validity = (token) => {
-    console.log(token)
-    let api = ''
+  const check_token_validity = async (token) => {
+    //console.log(token);
+    const api = `http://${process.env.REACT_APP_API_URL_DEV}/check_token/`;
     try {
-      api = 'http://' + process.env.REACT_APP_API_URL_DEV + `/check_token/`
-      axios.post(api, { token: token })
-        .then(response => {
-          console.log(response.data['response'])
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    
+        const response = await axios.post(api, { token: token });
+        console.log(response.data['status']); 
+        return response.data['status'];        
     } catch (error) {
-      console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error);
+        return null;  
     }
   }
 
@@ -114,119 +111,127 @@ export default function StartingForm() {
   }, [data]); // Dependency array ensures this effect runs only when 'data' changes
 
   useEffect(() => {
-    check_token_validity(token);
-  }, []); 
+    
+    check_token_validity(token).then(status => {
+        setIsValidRedirect(status);
+        console.log(status); // This will log the actual status ("False" in this case)
+    }).catch(error => {
+        console.error('Error validating token:', error);
+    });
+  }, []);
 
-  // if (!isValidRedirect) {
-  //   return (
-  //     <Alert 
-  //       severity="error"  
-  //       style={{ 
-  //         textAlign: 'center', 
-  //         marginTop: '0px', // Adds a top margin, adjust as needed
-  //       }}
-  //     >
-  //       <Typography variant="h6" component="div" style={{ fontSize: '1.25rem' }}>
-  //         Error: This page can't be accessible directly.
-  //       </Typography>
-  //     </Alert>
-  //   );
-  // }
-
-  return (
-    <>
-      <Box
-        component="form"
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          '& .MuiTextField-root': { m: 1, width: '25ch' },
+  if (isValidRedirect == 'False') {
+    return (
+      <Alert 
+        severity="error"  
+        style={{ 
+          textAlign: 'center', 
+          marginTop: '0px', // Adds a top margin, adjust as needed
         }}
-        noValidate
-        autoComplete="off"
       >
-        <Alert severity="info"  style={{ textAlign: 'center'}}>
-        This is a simple interface to test our new script to retrieve paper details based on the DOI alone. This script will be integrated into a new approach to collect iBET's scientific KPIs to save time that is best used doing actual research.<br /><br />
-        Thanks for testing! Any feedback is welcome. Please send an email to Pedro Cruz (pedro.cruz@ibet.pt), Ines Isidro (iaisidro@ibet.pt) and Mauro Oliveri (mauro.oliveri@ibet.pt) or reach out to any of us directly.
-        </Alert>
-        <img src={logos} alt="logo" width="150" height="80" style={{ float: 'left', marginRight: '1450px', paddingTop: '40px'  }} />
-        <div style={{ display: 'flex', alignItems: 'center', paddingBottom: '20px'  }}>
-          <Typography component="h1" variant="h3" style={{ fontFamily: 'Sedan-Regular', fontWeight: 400 }}>
-            Add Publication
-          </Typography>
-        </div>
-        <div style={{ display: 'flex', 
-                      gap: '10px',  
-                      width: '100vw',  
-                      justifyContent: 'center',
-                      paddingTop: '40px', 
-                      paddingBottom: '60px'}}> 
-          <div>
-            <Button variant="outlined" onClick={handleClickOpen}  
-                    style={{ 
-                      height: '56px', 
-                      margin: '0',
-                      width: '550px',
-                      padding: '0px 14px',
-                      color: 'rgba(0, 0, 0, 0.87)',
-                      borderColor: 'rgba(0, 0, 0, 0.23)'
-                      }}>
-              {textDialog}
-            </Button>
-            <MyDialog isOpen={open} handleClose={handleClose} onSelectionChangeDialog={setSelectedGroups}/>
+        <Typography variant="h6" component="div" style={{ fontSize: '1.25rem' }}>
+          Error: This page can't be accessible directly.
+        </Typography>
+      </Alert>
+    );
+  }
+
+  if (isValidRedirect == 'True') {
+    return (
+      <>
+        <Box
+          component="form"
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <Alert severity="info"  style={{ textAlign: 'center'}}>
+          This is a simple interface to test our new script to retrieve paper details based on the DOI alone. This script will be integrated into a new approach to collect iBET's scientific KPIs to save time that is best used doing actual research.<br /><br />
+          Thanks for testing! Any feedback is welcome. Please send an email to Pedro Cruz (pedro.cruz@ibet.pt), Ines Isidro (iaisidro@ibet.pt) and Mauro Oliveri (mauro.oliveri@ibet.pt) or reach out to any of us directly.
+          </Alert>
+          <img src={logos} alt="logo" width="150" height="80" style={{ float: 'left', marginRight: '1450px', paddingTop: '40px'  }} />
+          <div style={{ display: 'flex', alignItems: 'center', paddingBottom: '20px'  }}>
+            <Typography component="h1" variant="h3" style={{ fontFamily: 'Sedan-Regular', fontWeight: 400 }}>
+              Add Publication
+            </Typography>
           </div>
-          <TextField 
-          id="outlined-search" 
-          label="DOI *" 
-          type="search" 
-          style={{ width: '27%', height: '40px',margin: '0'}} 
-          value={doi} 
-          onChange={handleDoiChange}
-          error={!isValidDoi}
-          helperText={
-              <>
-                <Typography variant="body2" component="span">
-                    e.g.:
-                </Typography>
-                {' '}
-                <Typography variant="body2" component="span" style={{ fontWeight: 'bold' }}>
-                    10.1016/j.carbpol.2016.01.046
-                </Typography>
-              </>
-          }/>
-          <TextField 
-          id="outlined-search" 
-          label="Project(s)" 
-          type="search" 
-          style={{ width: '27%', height: '40px',margin: '0'}} 
-          value={projectCodes} 
-          onChange={(e) => setProjectCodes(e.target.value)} 
-          helperText={
-              <>
-                <Typography variant="body2" component="span">
-                  Project(s) associated with this KPI. Enter the project LabOrders codes, separated by commas, e.g.: <i><strong>P-123</strong>, <strong>PI-456</strong></i>
-                </Typography>
-              </>
-          }/>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'right', width: '83.5vw' }}>
-          {flag === 0 && (
-            <Link>
-              <Button type="submit" variant="contained" color="primary" onClick={handleApiTest} style={{ width: '15%', marginTop:'20px' }}>
-                Submit
+          <div style={{ display: 'flex', 
+                        gap: '10px',  
+                        width: '100vw',  
+                        justifyContent: 'center',
+                        paddingTop: '40px', 
+                        paddingBottom: '60px'}}> 
+            <div>
+              <Button variant="outlined" onClick={handleClickOpen}  
+                      style={{ 
+                        height: '56px', 
+                        margin: '0',
+                        width: '550px',
+                        padding: '0px 14px',
+                        color: 'rgba(0, 0, 0, 0.87)',
+                        borderColor: 'rgba(0, 0, 0, 0.23)'
+                        }}>
+                {textDialog}
               </Button>
-            </Link>
-            )}
-          {flag === 1 && (
-            <div style={{}}>
-              <SPLoader />
+              <MyDialog isOpen={open} handleClose={handleClose} onSelectionChangeDialog={setSelectedGroups}/>
             </div>
-          )}
-        </div>
-      </Box>
-      <Copyright sx={{ mt: 5 }} />
-    </>
-  );
+            <TextField 
+            id="outlined-search" 
+            label="DOI *" 
+            type="search" 
+            style={{ width: '27%', height: '40px',margin: '0'}} 
+            value={doi} 
+            onChange={handleDoiChange}
+            error={!isValidDoi}
+            helperText={
+                <>
+                  <Typography variant="body2" component="span">
+                      e.g.:
+                  </Typography>
+                  {' '}
+                  <Typography variant="body2" component="span" style={{ fontWeight: 'bold' }}>
+                      10.1016/j.carbpol.2016.01.046
+                  </Typography>
+                </>
+            }/>
+            <TextField 
+            id="outlined-search" 
+            label="Project(s)" 
+            type="search" 
+            style={{ width: '27%', height: '40px',margin: '0'}} 
+            value={projectCodes} 
+            onChange={(e) => setProjectCodes(e.target.value)} 
+            helperText={
+                <>
+                  <Typography variant="body2" component="span">
+                    Project(s) associated with this KPI. Enter the project LabOrders codes, separated by commas, e.g.: <i><strong>P-123</strong>, <strong>PI-456</strong></i>
+                  </Typography>
+                </>
+            }/>
+          </div>
+          <div style={{ display: 'flex', justifyContent: 'right', width: '83.5vw' }}>
+            {flag === 0 && (
+              <Link>
+                <Button type="submit" variant="contained" color="primary" onClick={handleApiTest} style={{ width: '15%', marginTop:'20px' }}>
+                  Submit
+                </Button>
+              </Link>
+              )}
+            {flag === 1 && (
+              <div style={{}}>
+                <SPLoader />
+              </div>
+            )}
+          </div>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </>
+    );
+  }
 }
