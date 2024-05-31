c828ef55acd55047903c565ca817f8e83233ca91
moliveri1618 <lastiada1@gmail.com>
2024-05-31 21:52:34 +0100

    commit

--- src/Components/SignInSide.jsx
+++ src/Components/SignInSide.jsx
@@ -210,7 +210,7 @@ export default function SignInSide() {
               <Grid container>
                 <Grid item xs>
                   <Link href="#" variant="body2">
-                    Forgot password?
+
                   </Link>
                 </Grid>
               </Grid>
