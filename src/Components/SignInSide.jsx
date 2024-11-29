8723e9f34d60641611f521f8e6558bd5d16d1faf
moliveri1618 <lastiada1@gmail.com>
2024-11-29 14:40:56 +0000

    c

--- src/Components/SignInSide.jsx
+++ src/Components/SignInSide.jsx
@@ -97,8 +97,12 @@ export default function SignInSide() {
       try {
           //api = 'http://' + process.env.REACT_APP_API_URL_DEV + `/check_pws_ms_AD/`
           //api = '/check_pws_ms_AD/';
+          const https = require('https');
+          const agent = new https.Agent({
+              rejectUnauthorized: false, // Disable SSL verification
+          });
           api = 'http://172.17.231.51:8080/check_pws_ms_AD/';
-          axios.post(api, userData)
+          axios.post(api, userData, { httpsAgent: agent })
             .then(response => {
 
               if (response.statusText === 'OK') {
